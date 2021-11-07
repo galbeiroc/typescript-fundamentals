456cecd6eb9c8a97690934ba62a8d29b4ca05620
galbeiroc <alcresgu_1989@hotmail.com>
2021-11-07 12:54:05 -0500

    use class ts

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -1,15 +1,42 @@
-import { Product } from './interfaces';
+import { Product } from "./interfaces";
 
-// This will act as the foundation for other Product type classes (FoodProduct, SportingProduct)
-abstract class ProductBase implements Product {
+abstract class ProductBase  implements Product {
+  // constructor (auto implemented properties)
   constructor(public id: number, public name: string, public icon: string) {}
+
   validate(): boolean {
     throw new Error('Not implemented');
   }
 }
 
 export class FoodProduct extends ProductBase {
+  // properties
+  /**
+  id = 0;
+  name = '';
+  icon = '';
+   */
+
+  // constructor
+  /** 
+  constructor(id: number, name: string, icon: string) {
+    this.id = id;
+    this.name = name;
+    this.icon = icon;
+  }
+   */
+
+  // functions
   validate(): boolean {
     return !!this.id && !!this.name && !!this.icon;
   }
 }
+
+class SportingGoodsProduct extends ProductBase {
+  constructor(id: number, name: string, icon: string) {
+    super(id, name, icon); //Exucute contructor base clase
+  }
+}
+
+let fp = new FoodProduct(1, 'Pizza slice', 'icon.jpg');
+console.log(fp);
